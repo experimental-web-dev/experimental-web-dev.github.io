a614eaf447467d5336c2af2940df28aaced7ee41
experimental.web.dev <experimental.web.dev@gmail.com>
2022-08-13 13:37:46 -0300

    DarkMode

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+const lightCollection = document.querySelectorAll(".light-mode");
+const toggle_dark_mode = document.querySelector(".toggle-dark-mode");
+
+var dark_mode = false;
+
+toggle_dark_mode.addEventListener("click", () => {
+	toggleTheme();
+});
+
+function isThemeSelected(){
+	return document.cookie.match(/theme=dark/i) != null;
+}
+
+function setTheme(dark){
+	dark_mode = dark;
+	console.log(dark_mode);
+	if (dark_mode){
+		for (const element of lightCollection){
+			element.classList.add("dark-mode");
+		}
+		document.cookie = "theme=dark";
+	}else{
+		for (const element of lightCollection){
+			element.classList.remove("dark-mode");
+		}
+		document.cookie = "theme=light";
+		console.log(document.cookie);
+	}
+	let allcookies = document.cookie;
+	console.log(allcookies);
+}
+
+function setThemeFromCookie(){
+	dark_mode = isThemeSelected();
+	setTheme(dark_mode);
+}
+
+function toggleTheme(){
+	setTheme(!dark_mode);
+}
+
+(function(){
+	document.cookie = "username=John Doe";
+	setThemeFromCookie();
+})();
