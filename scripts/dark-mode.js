c83bf4d8b1014708279b3ef0e37a8db8c6f13fe9
davidfrk <davida1d2@gmail.com>
2023-07-06 10:59:56 -0300

    Altering dark mode icon.

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -17,14 +17,14 @@ function setTheme(dark){
 		for (const element of lightCollection){
 			element.classList.add("dark-mode");
 		}
-		toggle_dark_mode.firstElementChild.className="far fa-moon";
+		toggle_dark_mode.firstElementChild.src="/imgs/moon.svg";
 		//document.cokie = "theme=dark; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 		document.cookie = "theme=dark; path=/";
 	}else{
 		for (const element of lightCollection){
 			element.classList.remove("dark-mode");
 		}
-		toggle_dark_mode.firstElementChild.className="far fa-sun";
+		toggle_dark_mode.firstElementChild.src="/imgs/sun.svg";
 		//document.cokie = "theme=light; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 		document.cookie = "theme=light; path=/";
 	}
