62fc3e5724ea756f16131cbd473cadff8532e952
davidfrk <davida1d2@gmail.com>
2023-07-30 14:25:53 -0300

    Disabling mouse move events when hovering over UI

--- src/raytracingAppUI.ts
+++ src/raytracingAppUI.ts
@@ -7,11 +7,42 @@ function hideUI() {
     }
 }
 
+function updateRangeValue(inputElement:HTMLInputElement) {
+    const value = inputElement.value
+    const nextSibling = (inputElement.nextElementSibling as HTMLElement)
+    if (parseFloat(inputElement.step) >= 1) {
+        nextSibling.textContent = parseFloat(value).toFixed(0)
+    } else {
+        nextSibling.textContent = parseFloat(value).toFixed(1)
+    }
+}
+
 function setup() {
     window.addEventListener('keydown', event => {
         if (event.key.toLowerCase() == 'h') {
             hideUI()
         }
+    })
+
+    let inputRangeList = document.querySelectorAll('input[type="range"]')
+    inputRangeList.forEach(element => {
+        element.addEventListener('input', event => {
+            updateRangeValue(event.target as HTMLInputElement)
+        })
+        updateRangeValue(element as HTMLInputElement)
+    });
+
+    let inputList = document.querySelectorAll('input')
+    inputList.forEach(element => {
+        element.addEventListener('mousemove', event => {
+            event.stopPropagation()
+        })
+    });
+
+    document.querySelectorAll('select').forEach(element => {
+        element.addEventListener('mousemove', event => {
+            event.stopPropagation()
+        })
     })
 }
 
